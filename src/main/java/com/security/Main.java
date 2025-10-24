6788b48a7f64ac0d287e77ec4b24135d4d68323f
denis <dkaras2016@gmail.com>
2025-10-24 12:24:58 +0500

    Added max x,y to map

--- src/main/java/com/security/Main.java
+++ src/main/java/com/security/Main.java
@@ -2,6 +2,9 @@ package com.security;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Map map = new Map();
+        map.setMaxCoordinates(50, 100);
+        System.out.println(map.getMAX_X());
+        System.out.println(map.getMAX_Y());
     }
 }
--- src/main/java/com/security/Map.java
+++ src/main/java/com/security/Map.java
@@ -1,11 +1,21 @@
 package com.security;
 
 import com.security.entity.Entity;
+import lombok.Getter;
 
 import java.util.HashMap;
 
 public class Map {
+    @Getter
+    private int MAX_X;
+    @Getter
+    private int MAX_Y;
     HashMap<Coordinates, Entity> cells = new HashMap<>();
+
+    public void setMaxCoordinates(int x, int y){
+        this.MAX_X = x;
+        this.MAX_Y = y;
+    }
 
     public void putEntity(Entity entity, Coordinates coordinates){
         cells.put(coordinates, entity);
